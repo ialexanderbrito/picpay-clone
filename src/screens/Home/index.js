f09e53f95e3b841975fd6e17ba09db1cf0e307d5
ialexanderbrito <xanderzinho26@gmail.com>
2020-05-31 14:32:16 -0300

    Finalizado Header

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import {
+  Wrapper,
+  Header,
+  BalanceContainer,
+  BalanceTitle,
+  Balance,
+} from './styles';
 
 export default function Home() {
-  return <Text> Home </Text>;
+  return (
+    <Wrapper>
+      <Header>
+        <MaterialCommunityIcons name="qrcode-scan" size={25} color="#10c86e" />
+
+        <BalanceContainer>
+          <BalanceTitle>Meu saldo</BalanceTitle>
+          <Balance>R$ 1.000,00</Balance>
+        </BalanceContainer>
+
+        <AntDesign name="gift" size={25} color="#10c86e" />
+      </Header>
+    </Wrapper>
+  );
 }
