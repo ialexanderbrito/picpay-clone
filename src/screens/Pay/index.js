55f22acafd3c68bc78d49e9daa89ded2543fd7f2
ialexanderbrito <xanderzinho26@gmail.com>
2020-06-01 17:30:09 -0300

    Finalizado page Pagamento

--- src/screens/Pay/index.js
+++ src/screens/Pay/index.js
@@ -1,6 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import Suggestions from '../../components/Suggestions';
+import Services from '../../components/Services';
+
+import { Wrapper, Container, Header, Input, TextInput, Text } from './styles';
 
 export default function Pay() {
-  return <Text> Pay </Text>;
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            size={25}
+            color="#00fc6c"
+          />
+
+          <Input>
+            <AntDesign name="search1" size={20} color="#fff" />
+            <TextInput
+              placeholder="Quem você quer pagar?"
+              keyboardAppearance="dark"
+            />
+          </Input>
+        </Header>
+
+        <Text>Sugestões para Você</Text>
+        <Suggestions />
+
+        <Services />
+      </Container>
+    </Wrapper>
+  );
 }
---
+++ src/screens/Pay/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.SafeAreaView`
+  flex: 1;
+  background: #000;
+`;
+
+export const Container = styled.ScrollView``;
+
+export const Header = styled.View`
+  height: 50px;
+  padding: 0 16px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Input = styled.View`
+  margin-left: 20px;
+  padding: 10px 20px;
+  flex: 1;
+  border-radius: 50px;
+  background-color: #1e232a;
+  border-width: 2px;
+  border-color: rgba(255, 255, 255, 0.6);
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const TextInput = styled.TextInput.attrs({
+  placeholderTextColor: '#AAAFBB',
+})`
+  margin: 0 20px;
+  flex: 1;
+`;
+
+export const Text = styled.Text`
+  margin: 10px 0 -10px;
+  padding: 20px 16px;
+  background-color: #1e232a;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
